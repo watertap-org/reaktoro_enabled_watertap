159a964aa59776be7d43ae4692fffa2138617fab
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2026-01-12 11:11:15 -0500

    Update test_softing_acid_ro.py

--- src/reaktoro_enabled_watertap/flowsheets/softening_acid_ro/tests/test_softing_acid_ro.py
+++ src/reaktoro_enabled_watertap/flowsheets/softening_acid_ro/tests/test_softing_acid_ro.py
@@ -18,7 +18,7 @@ def test_softening_acid_ro_seawater_with_hpro():
         bfgs_initialization_type="GaussNewton",
     )
     sar.initialize(m)
-    m.fs.water_recovery.fix(0.725)
+    m.fs.water_recovery.fix(0.75)
     sar.solve_model(m, tee=True)
     m.fs.water_recovery.fix(0.8)
     sar.solve_model(m, tee=True)
