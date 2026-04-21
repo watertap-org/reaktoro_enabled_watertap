8d356d6e436e285e6ab53132412e1ab160de0757
Carolina Tristan <ctristan@purdue.edu>
2026-04-21 17:05:15 -0400

    Reduce translator seawater recovery sweep to improve solver convergence

--- src/reaktoro_enabled_watertap/unit_models/tests/test_multi_comp_mvc.py
+++ src/reaktoro_enabled_watertap/unit_models/tests/test_multi_comp_mvc.py
@@ -145,7 +145,7 @@ def test_crystallizer_no_reaktoro_sea_water_prop_with_translators():
     m.fs.MVC.recovery.fix()
     assert degrees_of_freedom(m) == 4
     m.fs.cost_objective = Objective(expr=m.fs.costing.LCOW)
-    for r in [0.5, 0.8]:
+    for r in [0.5, 0.75]:  # 0.8 causes convergence issues
 
         m.fs.MVC.recovery.fix(r)
         solver = get_cyipopt_watertap_solver(linear_solver="mumps", max_iter=3000)
